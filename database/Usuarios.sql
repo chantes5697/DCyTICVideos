ae7b78b31b99bd164a82927bafdd1b636739211c
chantes5697 <rodrigo.chantes@gmail.com>
2020-11-17 17:02:26 -0600

    rol

--- database/Usuarios.sql
+++ database/Usuarios.sql
@@ -1,6 +1,24 @@
 USE [Videos]
 GO
 
+/****** Object:  Table [dbo].[Rol]    Script Date: 11/17/2020 17:00:21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Rol](
+	[id_rol] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [text] NOT NULL,
+ CONSTRAINT [PK_Rol] PRIMARY KEY CLUSTERED 
+(
+	[id_rol] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
 /****** Object:  Table [dbo].[Usuarios]    Script Date: 11/5/2020 11:22:39 ******/
 SET ANSI_NULLS ON
 GO
