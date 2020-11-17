b66157de00119b8734d7f453a998ec92bd3b9848
chantes5697 <rodrigo.chantes@gmail.com>
2020-11-17 15:30:45 -0600

    database usuarios

---
+++ database/Usuarios.sql
@@ -0,0 +1,37 @@
+USE [Videos]
+GO
+
+/****** Object:  Table [dbo].[Usuarios]    Script Date: 11/5/2020 11:22:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Usuario](
+	[id_usuario] [int] IDENTITY(1,1) NOT NULL,
+	[username] [text] NOT NULL,
+	[rol] [int] NOT NULL,
+	[password] [varchar](255) NOT NULL,
+ CONSTRAINT [PK_Usuario] PRIMARY KEY NONCLUSTERED 
+(
+	[id_usuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[Usuario]  WITH CHECK ADD  CONSTRAINT [FK_Usuario_Rol] FOREIGN KEY([rol])
+REFERENCES [dbo].[Rol] ([id_rol])
+GO
+
+ALTER TABLE [dbo].[Usuario] CHECK CONSTRAINT [FK_Usuario_Rol]
+GO
+
+
